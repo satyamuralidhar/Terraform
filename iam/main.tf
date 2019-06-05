134aa77322b93d8473bc3253641ab526f41bcec0
muralidhar <immurali5@gmail.com>
2019-06-05 14:16:35 +0530

    iam

---
+++ iam/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  access_key = "${var.accesskey}"
+  secret_key = "${var.secretkey}"
+  region     = "${var.location}"
+}
+
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "lb" {
+  user = "${aws_iam_user.lb.name}"
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "satya"
+  user = "${aws_iam_user.lb.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
---
+++ iam/var.tf
@@ -0,0 +1,11 @@
+variable "accesskey" {
+  
+}
+variable "secretkey" {
+  
+}
+
+
+variable "location" {
+  default = "ap-south-1"
+}
