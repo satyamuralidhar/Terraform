42ffcd6dc39587124296d15575c13ac452dbd9b8
muralidhar <38804803+immurali5@users.noreply.github.com>
2019-06-05 13:03:23 +0530

    Create var.tf

---
+++ aws codebuild/var.tf
@@ -0,0 +1,33 @@
+variable "accesskey" {
+  
+}
+
+variable "secretkey" {
+  
+}
+variable "location" {
+  default = "ap-south-1"
+}
+
+variable "github" {
+  default = "https://github.com/immurali5/Smartshop.git"
+}
+variable "subnet1" {
+  default = "subnet-547989465a36"
+}
+variable "subnet2" {
+  default = "subnet-6dh7878891"
+}
+variable "securitygroup" {
+  default = "sg-09a58844a12"
+}
+variable "vpc" {
+  default = "vpc-f123456789"
+}
+
+
+
+
+
+
+
